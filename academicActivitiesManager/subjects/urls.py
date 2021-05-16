a227f518c0938fb2fdc1bf39b5fc49ee1096d849
AriasPaez <ariaspaez.dev@gmail.com>
2021-05-16 14:28:49 -0500

    [feat] details css and parse data from database in template

--- academicActivitiesManager/subjects/urls.py
+++ academicActivitiesManager/subjects/urls.py
@@ -4,4 +4,5 @@ from subjects import views
 
 urlpatterns = [
     path('index/', views.index, name="index"),
+    path('finish_activity/<str:id_activity>', views.update_activity, name="finish_activity"),
 ]
--- academicActivitiesManager/subjects/views.py
+++ academicActivitiesManager/subjects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Activity, Subject, TypeActivity
 
@@ -13,3 +13,13 @@ def index(request):
         'type_activities': all_type_activities
     }
     return render(request, 'subjects/index.html', context)
+
+
+def update_activity(request, id_activity):
+    name_grade = request.POST['grade']
+    name_date_finished = request.POST['date_finished']
+    print('ID_ACTIVITY', id_activity)
+    print('NAME_GRADE', name_grade)
+    print('name_date_finished', name_date_finished)
+
+    return redirect("index")
